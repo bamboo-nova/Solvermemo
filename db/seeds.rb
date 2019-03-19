6dc4f9169963094516bd47636a1e26d767aa62f5
bamboo <suzuk1nov11@gmail.com>
2019-03-19 08:50:34 +0000

    make tag seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tag.create(name: '機械学習')
+Tag.create(name: '最適化')
+Tag.create(name: 'スクレイピング')
+Tag.create(name: 'データ分析')
+Tag.create(name: 'アプリ開発')
+Tag.create(name: 'API')
+Tag.create(name: 'ライブラリ・パッケージ')
+Tag.create(name: 'ディープラーニング')
+Tag.create(name: 'デバッグ')
+Tag.create(name: 'セキュリティ')
+
+

5c3f542bf75823591152465fc0115e01f19ee0c6
bamboo <suzuk1nov11@gmail.com>
2019-03-30 13:41:36 +0000

    mecab heroku challenge ver2

--- db/migrate/20190323084157_change_data_parent_user_id_to_comments.rb
+++ db/migrate/20190323084157_change_data_parent_user_id_to_comments.rb
@@ -1,5 +1,5 @@
 class ChangeDataParentUserIdToComments < ActiveRecord::Migration[5.0]
   def change
-    change_column :comments, :parent_user_id, :int
+    change_column :comments, :parent_user_id, :integer
   end
 end
